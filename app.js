4595a036cf03d48be9abdb1d958ccac35ef2bd53
JackArt237483 <vip.artem4567@mail.ru>
2023-06-10 10:07:49 +0300

    second commit

--- app.js
+++ app.js
@@ -17,3 +17,33 @@ function moveChildBlock(distance, duraction) {
 }
 
 moveChildBlock(10,1000) // здесь настройки для дочернего блока
+
+
+// 2) task
+
+
+function validateForm() {
+ 
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var repeatPassword = document.getElementById("repeat_password").value;
+
+  var emailRegex = /^[\w.-]{4,16}@gmail\.com$/;
+  var passwordRegex = /^[A-Z][a-zA-Z0-9]{4,16}$/;
+
+  if (!emailRegex.test(email)) {
+    alert("Введите действительный адрес электронной почты Gmail");
+    return false;
+  }
+
+  if (!passwordRegex.test(password)) {
+    alert("Пароль должен начинаться с большой буквы, содержать только буквы и цифры, и иметь длину от 4 до 16 символов");
+    return false;
+  }
+
+  if (password !== repeatPassword) {
+    alert("Пароли не совпадают");
+    return false;
+  }
+}
